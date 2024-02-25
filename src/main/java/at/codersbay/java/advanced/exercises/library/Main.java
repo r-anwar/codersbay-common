6544c6c2206a757003fa8542ae5bd69b3b7c8610
Reza Anwar <reza@anwar.at>
2024-02-25 15:22:51 +0100

    - added book exercise

--- src/main/java/at/codersbay/java/advanced/exercises/library/Main.java
+++ src/main/java/at/codersbay/java/advanced/exercises/library/Main.java
@@ -1,11 +1,13 @@
 package at.codersbay.java.advanced.exercises.library;
 
 /**
- * Erstelle ein Buchverwaltungssystem. Das Buchverwaltungssystem soll eine Liste von Bücher halten, desweiteren soll
- * der User Bücher nach deren Titel, ISBN oder nach dem Vor- und Nachnamen des Autors suchen/finden können.
+ * Erstelle ein Buchverwaltungssystem.
+ * Das Buchverwaltungssystem soll eine Liste von Bücher halten, des Weiteren soll
+ * nach Büchern anhand derer Titel oder ISBN oder dem Vor- und Nachnamen des Autors gesucht werden können.
  *
  * Ein Buch hat folgende Attribute (Titel, ISBN, Autor-Objekt)
  * Ein Autor besteht aus folgenden Attributen (Vorname, Nachname)
+ *
  */
 public class Main {
 

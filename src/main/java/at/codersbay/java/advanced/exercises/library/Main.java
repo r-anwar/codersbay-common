b2c03429a574dba5e054d6671d358402f29acb70
Reza Anwar <reza@anwar.at>
2024-02-26 16:14:04 +0100

    - added book exercise

--- src/main/java/at/codersbay/java/advanced/exercises/library/Main.java
+++ src/main/java/at/codersbay/java/advanced/exercises/library/Main.java
@@ -4,9 +4,15 @@ package at.codersbay.java.advanced.exercises.library;
  * Erstelle ein Buchverwaltungssystem.
  * Das Buchverwaltungssystem soll eine Liste von Bücher halten, des Weiteren soll
  * nach Büchern anhand derer Titel oder ISBN oder dem Vor- und Nachnamen des Autors gesucht werden können.
+ * Bücher sollen ausgeliehen und zurückgegeben werden können, sind die Bücher ausgeliehen so darf das Buch kein weiteres
+ * mal ausgeliehen werden.
  *
- * Ein Buch hat folgende Attribute (Titel, ISBN, Autor-Objekt)
+ * Ein Buch hat unter anderen(!) folgende Attribute (Titel, ISBN, Autor-Objekt)
  * Ein Autor besteht aus folgenden Attributen (Vorname, Nachname)
+ *
+ *
+ * Erstellen Sie mindestens 5 Bücher, mindestens 2 Bücher sollen denselben Autor haben. Suchen Sie nach mindestens
+ * 2 Büchern, leihen Sie mindestens 2 Bücher aus und geben Sie diese auch wieder zurück.
  *
  */
 public class Main {

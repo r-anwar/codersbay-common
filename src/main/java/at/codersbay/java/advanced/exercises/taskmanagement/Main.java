3633e10b9d4cb336b75a573f4083e831bd83680d
reza@anwar.at <reza@anwar.at>
2024-02-28 18:44:49 +0100

    - exercise Book added Implementation.

---
+++ src/main/java/at/codersbay/java/advanced/exercises/taskmanagement/Main.java
@@ -0,0 +1,16 @@
+package at.codersbay.java.advanced.exercises.taskmanagement;
+
+/**
+ * Erstelle ein Taskverwaltungssystem. Das Taskverwaltungssystem besteht aus einer Liste von Aufgaben (Task).
+ *
+ * Eine Aufgabe hat eine ID einen Titel, eine Beschreibung einen Status-Erledigt (true|false).
+ *
+ * Das Taskverwaltungssystem hat eine Methode die einen Task der Taskliste hinzufügt.
+ * Das Taskverwaltungssystem hat eine Methode die anhand der ID ein Task aus der Taskliste entfernt.
+ * Das Taskverwaltungssystem hat eine Methode die ein Task abgeschliesst.
+ * Das Taskverwaltungssystem hat eine Methode die alle Tasks vom Taskverwaltungssystem anzeigt.
+ *
+ */
+public class Main {
+
+}

cc8ca07436db6fe97a8465e024aff99e0300b1b9
Reza Anwar <reza@anwar.at>
2024-03-02 07:17:39 +0100

    - added two new exercises

---
+++ src/main/java/at/codersbay/java/advanced/exercises/lottery/Main.java
@@ -0,0 +1,17 @@
+package at.codersbay.java.advanced.exercises.lottery;
+
+/**
+ * Erstellen Sie das Spiel Lotto 6 aus 45.
+ *
+ * Lesen Sie 6 Zahlen aus kommasepariert über die Kommandozeile ein. Losen Sie mittels der Klasse
+ * java.util.Random 6 Zahlen aus 45 aus. Geben Sie das Ergebnis über die Kommandozeile aus,
+ * des Weiteren ob der Benutzer einen Gewinn erzielt hat oder nicht.
+ *
+ * Folgende Gewinnklassen gibt es.
+ * - 3 gleiche Zahlen
+ * - 4 gleiche Zahlen
+ * - 5 gleiche Zahlen
+ * - 6 gleiche Zahlen
+ */
+public class Main {
+}
---
+++ src/main/java/at/codersbay/java/advanced/exercises/steinScherePapier/Main.java
@@ -0,0 +1,15 @@
+package at.codersbay.java.advanced.exercises.steinScherePapier;
+
+/**
+ * Schreiben Sie ein Programm welches das ein Spiel "Stein, Schere, Papier" simuliert.
+ *
+ * Schritte: *
+ *     Definieren Sie ein enum-Typ mit den Namen Wahl und den drei Werten STEIN, SCHERE und PAPIER.
+ *     Implementieren Sie die Spiellogik in einer Methode spielRunde(Wahl benutzerWahl),
+ *     die die Wahl des Benutzers und des Computers vergleicht und den Gewinner bestimmt.
+ *     Verwenden Sie die Scanner-Klasse, um die Wahl des Benutzers einzugeben.
+ *     Generieren Sie die Wahl des Computers zufällig mittels der Klasse java.util.Random
+ *     Geben Sie das Ergebnis der Spielrunde und den Gewinner aus.
+ */
+public class Main {
+}

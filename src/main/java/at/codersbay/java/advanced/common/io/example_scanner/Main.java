ab76990e909d74335204fa64fa0ae839d0796118
reza@anwar.at <reza@anwar.at>
2024-03-02 09:50:24 +0100

    - added catch NumberFormatException

--- src/main/java/at/codersbay/java/advanced/common/io/example_scanner/Main.java
+++ src/main/java/at/codersbay/java/advanced/common/io/example_scanner/Main.java
@@ -1,15 +1,52 @@
 package at.codersbay.java.advanced.common.io.example_scanner;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter username");
+    public static int maxInput = 5;
 
-        String userName = scanner.nextLine();
-        System.out.println("Username is: " + userName);
+    public static void main(String[] args) {
+        Scanner scanner = null;
+
+        try {
+
+            scanner = new Scanner(System.in);
+            readInput(scanner, 1);
+
+        } finally {
+            System.out.println("Ich bin im finally Block.");
+            if (scanner != null) {
+                try {
+                    scanner.close();
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void readInput(Scanner scanner, int loops) {
+        System.out.println("Enter Number");
+
+        String number = scanner.nextLine();
+
+        String[] numbers = number.split(",");
+
+        for (String tmpNumber : numbers) {
+
+            try {
+                int myNumber = Integer.parseInt(tmpNumber);
+                System.out.println("Number is: " + myNumber);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Sie haben keine Zahl eingegeben.");
+            }
+        }
+        if (loops < maxInput) {
+            loops += 1;
+            readInput(scanner, loops);
+        }
     }
 
 }
